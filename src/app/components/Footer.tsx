53ff04dbbadbf6e91ac0ab5308012caba4911a54
Paweł Chudecki <pawelc29@interia.pl>
2024-04-21 11:04:36 +0200

    confetti size fix

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,25 @@
+const Footer = () => {
+  return (
+    <footer className="absolute mx-auto w-screen left-0 bottom-0 text-center font-bold text-base text-MarineBlue p-4 max-md:p-4 max-md:text-xs ">
+      Challenge by
+      <a
+        className="text-PurplishBlue ml-1 hover:text-StrawberryRed  hover:underline"
+        href="https://www.frontendmentor.io/challenges/multistep-form-YVAnSdqQBJ"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by
+      <a
+        className="text-PurplishBlue ml-1 hover:text-StrawberryRed hover:underline"
+        href="https://www.frontendmentor.io/profile/SoulRvr29"
+        target="_blank"
+      >
+        Paweł Chudecki
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
